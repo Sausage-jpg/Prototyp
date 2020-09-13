e3f65c18be64d05f152b2371dc6205a3f35cf3b7
amazing_username <cojaturobie6@gmail.com>
2020-09-13 13:55:37 +0200

    13.09.2020 13:53

---
+++ Prototyp/Assets/EnemyBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public int health;
+    
+    public GameObject bullet;
+
+     
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <=0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Prototyp/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    private void Update()
+    {
+         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+            Debug.Log("Jump");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+
+
+
+}
+
+/**void Update()
+{
+    Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    moveVelocity = moveInput.normalized * movementSpeed;
+}
+
+private void FixedUpdate()
+{
+    rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+}*/
